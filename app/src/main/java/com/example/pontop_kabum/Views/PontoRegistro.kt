e6cb777c5b94a9d911e1c1918c37444a3175d42c
Erik <erik.mendes@kabum.com.br>
2021-08-08 22:49:14 -0300

    Feito melhorias no design da tela de ponto Adicionado Picasso para trazer a foto do email Colocado informações do usuário na tela de ponto

--- app/src/main/java/com/example/pontop_kabum/Views/PontoRegistro.kt
+++ app/src/main/java/com/example/pontop_kabum/Views/PontoRegistro.kt
@@ -3,24 +3,45 @@ package com.example.pontop_kabum.Views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import com.example.pontop_kabum.R
+import com.google.firebase.auth.FirebaseAuth
+import com.squareup.picasso.Picasso
+import org.w3c.dom.Text
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.concurrent.thread
 
 class PontoRegistro : AppCompatActivity() {
+    var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ponto_registro)
+
+        val dataAtual = findViewById<TextView>(R.id.dataCounter)
+        dataAtual.text = SimpleDateFormat("dd/MM/yyyy", Locale.ROOT).format(Date())
+
         countTimer()
         registrarPonto()
+        userInfo()
+    }
 
+    fun userInfo(){
+        val userInfo = FirebaseAuth.getInstance().currentUser
+        val nome = findViewById<TextView>(R.id.username)
+        val foto = findViewById<ImageView>(R.id.photoUrl)
+
+        nome.text = userInfo?.displayName
+
+        val photoUrl = userInfo?.photoUrl.toString()
+        Picasso.get().load(photoUrl).into(foto)
     }
 
     fun countTimer(){
         val timer = Timer()
-        timer?.scheduleAtFixedRate(object : TimerTask() {
+        timer.scheduleAtFixedRate(object : TimerTask() {
             override fun run() {
                 updateTimer()
             }
@@ -36,12 +57,32 @@ class PontoRegistro : AppCompatActivity() {
 
     fun registrarPonto(){
 
-        //TODO: COLOCAR CONTADOR PARA IDENTIFICAR QUAIS OS PONTOS ESTÃO SENDO BATIDOS
-
-        val registrar = findViewById<Button>(R.id.registrarPonto)
+        val registrar = findViewById<ImageButton>(R.id.registrarPonto)
         val registroEntrada = findViewById<TextView>(R.id.registroEntrada)
+        val entradaAlmoco = findViewById<TextView>(R.id.entradaAlmoco)
+        val saidaAlomoco = findViewById<TextView>(R.id.voltaAlmoco)
+        val saida = findViewById<TextView>(R.id.saida)
         registrar.setOnClickListener(){
-            registroEntrada.text = SimpleDateFormat("'Entrada: 'hh:mm:ss", Locale.ROOT).format(Date())
+            count++
+
+            if(count == 1){
+                registroEntrada.text = SimpleDateFormat("'Entrada: 'hh:mm", Locale.ROOT)
+                    .format(Date())
+            }
+            if(count == 2){
+                entradaAlmoco.text = SimpleDateFormat("'Entrada Almoço: 'hh:mm", Locale.ROOT)
+                    .format(Date())
+            }
+            if(count == 3){
+                saidaAlomoco.text = SimpleDateFormat("'Saída Almoço: 'hh:mm", Locale.ROOT)
+                    .format(Date())
+            }
+            if(count == 4){
+                saida.text = SimpleDateFormat("'Saída: 'hh:mm", Locale.ROOT)
+                    .format(Date())
+            } else {
+                !registroEntrada.isEnabled
+            }
         }
     }
 }
